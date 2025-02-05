8fca8a6cba52fd354b6f6fef205e5a4f4e4c40c5
James Hatfield <james.hatfield17@icloud.com>
2025-02-05 20:15:36 +0000

    Add initial array and functions to JS file, including object constructor

---
+++ script.js
@@ -0,0 +1,14 @@
+// Stores list of books
+const myLibrary = [];
+
+// Constuctor for book objects
+function Book(title, author, pages) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+}
+
+// Function to build book
+function addBookToLibrary() {
+
+}
